a378e1dcd3faea62099456fec44b6c3bcecae3ec
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-08-16 09:55:29 -0700

    range 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,8 +20,16 @@ public class Main {
 
         imageCodecs.imwrite("hsv.png", img);
 
-        Scalar upper = new Scalar(240, 184, 250);
-        Scalar lower = new Scalar(15, 87, 90);
+        Scalar upper = new Scalar(140, 144, 250);
+        Scalar lower = new Scalar(85, 84, 60);
+
+        Mat color = Mat.zeros(300, 300, CvType.CV_8UC3);
+        color.setTo(lower);
+        Imgproc.cvtColor(color, color, Imgproc.COLOR_HSV2BGR);
+        imageCodecs.imwrite("lower.png", color);
+        color.setTo(upper);
+        Imgproc.cvtColor(color, color, Imgproc.COLOR_HSV2BGR);
+        imageCodecs.imwrite("upper.png", color);
 
         Mat rangeMat = new Mat();
         Core.inRange(img, lower, upper, rangeMat);
